bddfb9532f6fcd74fde714bbdc3c470593dc9d5c
Chinedum Robert-Maduekwe <chinedumrobert-maduekwe@216.dhcp.apogeetelecom.com>
2015-12-26 15:03:19 -0700

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Chinedum Robert-Maduekwe on 12/26/15.
+//  Copyright © 2015 nedu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
